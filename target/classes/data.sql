985e3df1a8818af514f33548275618464f1b90c7
TanJiunKoon <jtan0374@student.monash.edu>
2024-12-18 16:00:39 +0800

    Separately push all the files into github repository.

---
+++ target/classes/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO publisher (name, location) VALUES ('Reilly Media', 'USA');
+INSERT INTO publisher (name, location) VALUES ('Penguin Books', 'UK');
+
+INSERT INTO book (title, author, pages, publisher_id) VALUES ('Spring in Action', 'Craig Walls', 400, 1);
+INSERT INTO book (title, author, pages, publisher_id) VALUES ('Hibernate Tips', 'Thorben Janssen', 200, 1);
+INSERT INTO book (title, author, pages, publisher_id) VALUES ('Clean Code', 'Robert C. Martin', 300, 2);
